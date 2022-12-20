0453a2965b43bdd784173f3ea63cb23f7a323e18
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2022-12-20 11:17:25 +0100

    :D

---
+++ pages/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-main>
+        <v-container>
+            <v-btn loading>Simple Button</v-btn>
+            <v-card>
+                <v-card-title>Card Title</v-card-title>
+                <v-card-subtitle>Card Subtitle</v-card-subtitle>
+                <v-card-text>Card Text</v-card-text>
+                <v-card-actions>
+                    <v-btn loading>Simple Button</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-container>
+    </v-main>
+</template>
